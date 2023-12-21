a790aa6b07ced9f87c312ecac1ac51c33654a5fd
Thorben <thorben@ascky.de>
2023-12-21 21:00:48 +0100

    feat: login, get addon install links

--- index.js
+++ index.js
@@ -9,14 +9,38 @@ if (!process.env.SITE_URL) {
   return;
 }
 
-(async () => {
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
-  await page.goto(process.env.SITE_URL);
+let availableAddonLinks = [];
 
-  /**
-   * TODO: Write code ;)
-   */
+(async () => {
+  const backendUrl = new URL('/redaxo', process.env.SITE_URL).href;
+  const browser = await chromium.launch({
+      headless: false,
+    }
+  );
+  const page = await browser.newPage();
+  await page.goto(backendUrl);
+
+  // Login
+  await login(page);
+
+  // Go to installer
+  await getAvailableAddons(page);
 
   await browser.close();
 })();
+
+async function login (page) {
+  await page.fill('#rex-id-login-user', process.env.USER);
+  await page.fill('#rex-id-login-password', process.env.PASSWORD);
+  await page.locator('button[type=submit]').click();
+}
+
+async function getAvailableAddons (page) {
+  const installerUrl = new URL('/redaxo/index.php?page=install/packages/add', process.env.SITE_URL).href;
+  await page.goto(installerUrl);
+
+  availableAddonLinks = await page.$$eval('tr td:nth-child(2) a', as => as.map(a => a.href));
+  console.log(availableAddonLinks);
+}
+
+// TODO: Test addons
